d6201d19947d05ecf76e94f47d539ecedd2da57d
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-12 11:42:18 +0600

    setup for login

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import { Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 
 export const jwtContext = React.createContext();
+export const apiUrlContext = React.createContext();
 
 function App() {
   const [jwt, setJwt] = useState("sometoken");
+  const apiUrl = "https://localhost:7190/";
 
   return (
     <>
       <jwtContext.Provider value={jwt}>
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login setJwt={setJwt} />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
+        <apiUrlContext.Provider value={apiUrl}>
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login setJwt={setJwt} />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </apiUrlContext.Provider>
       </jwtContext.Provider>
     </>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,37 @@
-import { useContext } from "react";
-import { jwtContext } from "../../App";
+import { useContext, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { jwtContext, apiUrlContext } from "../../App";
 import "../loginregstyles.css";
 
-export default function Login({ setJwt }) {
+const Login = ({ setJwt }) => {
   const jwt = useContext(jwtContext);
+  const apiUrl = useContext(apiUrlContext);
+
+  const [error, setError] = useState("");
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    // setError("Some dum error");
+  }, []);
+
   return (
     <div className="login-container">
       <h3>Login</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
+          <p style={errorStyle}>{error.length > 0 ? `${error}` : ""}</p>
           <label htmlFor="UserName">Username: </label>
           <input
             id="UserName"
             className="login-name-box"
             placeholder="username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div>
@@ -23,6 +41,8 @@ export default function Login({ setJwt }) {
             className="login-pass-box"
             type="password"
             placeholder="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
           />
         </div>
         <input className="login-button" type="submit" value="Sign in" />
@@ -32,4 +52,13 @@ export default function Login({ setJwt }) {
       </p>
     </div>
   );
-}
+};
+
+const errorStyle = {
+  color: "black",
+  textShadow: "red 0 0 2px",
+  margin: "0",
+  padding: "0",
+};
+
+export default Login;
