16a8d5dab03135815fbb791a1849289b5a5b0fbb
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-17 09:10:30 +0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>Home</h1>;
-}
+const Home = () => {
+  return (
+    <>
+      <div className="home-main-img">
+        <span className="home-title">SpicyCat's Blog</span>
+      </div>
+      <div className="home-container">{/* page numbers */}</div>
+    </>
+  );
+};
+
+export default Home;
