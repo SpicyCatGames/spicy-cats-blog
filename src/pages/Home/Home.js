8ebc2f17642f128dd0f5b92681a44dcd7b083e0a
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-17 11:05:08 +0600

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,25 @@
 import "./homestyles.css";
+import BackgroundImage from "./fine.png";
 
 const Home = () => {
+  const bgImg = {
+    backgroundImage: `url(${BackgroundImage})`,
+  };
+
   return (
-    <>
-      <div className="home-main-img">
+    <div className="home">
+      <div className="home-main-img" style={bgImg}>
         <span className="home-title">SpicyCat's Blog</span>
       </div>
-      <div className="home-container">{/* page numbers */}</div>
-    </>
+      <div className="home-container">
+        {/* page numbers */}
+        <a href="./" className="home-post">
+          <img src="/placeholders/comp.jpg" width="500" alt="" />
+          <span className="home-post-title">Post Title</span>
+        </a>
+        {/* page numbers */}
+      </div>
+    </div>
   );
 };
 
