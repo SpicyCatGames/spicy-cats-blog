c62cecb433ac2af3028bf1ba3decdb3797b57475
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-17 12:46:47 +0600

    fetch posts from server

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import "./homestyles.css";
 import BackgroundImage from "./fine.png";
+import { apiUrlContext } from "../../App";
 
 const pageSizeOptions = [
   { value: "3", label: "3" },
@@ -8,8 +9,39 @@ const pageSizeOptions = [
   { value: "10", label: "10" },
 ];
 
+// https://localhost:44362/api/Posts/getposts?pageNum=1&postsPerPage=10&category=a
+
 const Home = () => {
+  const apiUrl = useContext(apiUrlContext);
   const [pageSize, setPageSize] = useState("3");
+  const [pageNum, setPageNum] = useState("1"); //eslint-disable-line no-unused-vars
+  const [category, setCategory] = useState(""); //eslint-disable-line no-unused-vars
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postsFromServer = fetchPosts();
+    setPosts(postsFromServer);
+  }, [pageSize]); //eslint-disable-line react-hooks/exhaustive-deps
+
+  const fetchPosts = async () => {
+    const res = await fetch(
+      `${apiUrl}api/Posts/getposts?${pageNum}=1&postsPerPage=${pageSize}&category=${category}`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    } else {
+      //
+    }
+  };
 
   const handlePageSizeChange = (e) => {
     if (!(e.target.value === pageSize)) {
@@ -34,7 +66,7 @@ const Home = () => {
           ))}
         </select>
         {/* page numbers */}
-        <a href="./" className="home-post">
+        <a href="./" className="home-post" data-postid="">
           <img src="/placeholders/comp.jpg" width="500" alt="" />
           <span className="home-post-title">Post Title</span>
         </a>
