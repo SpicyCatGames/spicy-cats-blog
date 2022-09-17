3fcccd11a9badccd986703c7e812bab933cc8832
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-17 11:32:13 +0600

    add posts per page dropdown in Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./homestyles.css";
 import BackgroundImage from "./fine.png";
 
+const pageSizeOptions = [
+  { value: "3", label: "3" },
+  { value: "5", label: "5" },
+  { value: "10", label: "10" },
+];
+
 const Home = () => {
+  const [pageSize, setPageSize] = useState("3");
+
+  const handlePageSizeChange = (e) => {
+    if (!(e.target.value === pageSize)) {
+      setPageSize(e.target.value);
+    }
+  };
+
   const bgImg = {
     backgroundImage: `url(${BackgroundImage})`,
   };
@@ -12,6 +27,12 @@ const Home = () => {
         <span className="home-title">SpicyCat's Blog</span>
       </div>
       <div className="home-container">
+        Posts per Page:
+        <select onChange={handlePageSizeChange}>
+          {pageSizeOptions.map((option) => (
+            <option key={option.value}>{option.label}</option>
+          ))}
+        </select>
         {/* page numbers */}
         <a href="./" className="home-post">
           <img src="/placeholders/comp.jpg" width="500" alt="" />
