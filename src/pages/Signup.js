f48ac2523d11373058d3477113a28e0a18ce5611
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-11 10:45:32 +0600

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,29 +1,72 @@
 import "./loginregstyles.css";
+import { useContext, useState } from "react";
+import { apiUrlContext, loggedInContext } from "../App";
 
-export default function Signup() {
+const Signup = () => {
+  const loggedIn = useContext(loggedInContext);
+  const apiUrl = useContext(apiUrlContext);
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const signupInfo = {
+      username: name,
+      password: password,
+    };
+
+    signUp(signupInfo);
+  };
+
+  const signUp = async (signupInfo) => {
+    const res = await fetch(`${apiUrl}api/Auth/register`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "text/plain",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signupInfo),
+    });
+  };
+
   return (
     <div className="signup-container">
-      <h3>Create New Account</h3>
-      <form>
-        <div>
-          <label htmlFor="UserName">Username: </label>
-          <input
-            id="UserName"
-            className="signup-name-box"
-            placeholder="username"
-          />
-        </div>
-        <div>
-          <label htmlFor="Password">Password: </label>
-          <input
-            id="Password"
-            className="signup-pass-box"
-            type="password"
-            placeholder="password"
-          />
-        </div>
-        <input className="signup-button" type="submit" value="Sign Up" />
-      </form>
+      {loggedIn ? (
+        <h3>You are already logged in!</h3>
+      ) : (
+        <>
+          <h3>Create New Account</h3>
+          <form onSubmit={onSubmit}>
+            <div>
+              <label htmlFor="UserName">Username: </label>
+              <input
+                id="UserName"
+                className="signup-name-box"
+                placeholder="username"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="Password">Password: </label>
+              <input
+                id="Password"
+                className="signup-pass-box"
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <input className="signup-button" type="submit" value="Sign Up" />
+          </form>
+        </>
+      )}
     </div>
   );
-}
+};
+
+export default Signup;
