fc5c8184688e41334cf8fa54c62694b51865ef5e
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-23 19:34:30 +0600

    Update NewPost.js

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Tiptap from "../components/Tiptap";
 
 const NewPost = () => {
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState("");
+
   const onSubmit = (e) => {
     e.preventDefault();
   };
@@ -8,13 +12,23 @@ const NewPost = () => {
   return (
     <form onSubmit={onSubmit}>
       <label htmlFor="post-title">
-        <input type="text" id="post-title" placeholder="Title" />
+        <input
+          type="text"
+          id="post-title"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </label>
       <Tiptap />
       <span>Image upload will be added later</span>
+      <br />
       <span>Descrription field will be added later</span>
+      <br />
       <span>Tag field will be added later</span>
+      <br />
       <span>Category field will be added later</span>
+      <br />
     </form>
   );
 };
