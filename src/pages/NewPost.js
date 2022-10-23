c508d896bf33cd5ac2ead3f5f1b2b5b8e108ed8d
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-23 19:16:05 +0600

    Update NewPost.js

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,7 +1,22 @@
 import Tiptap from "../components/Tiptap";
 
 const NewPost = () => {
-  return <Tiptap />;
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="post-title">
+        <input type="text" id="post-title" placeholder="Title" />
+      </label>
+      <Tiptap />
+      <span>Image upload will be added later</span>
+      <span>Descrription field will be added later</span>
+      <span>Tag field will be added later</span>
+      <span>Category field will be added later</span>
+    </form>
+  );
 };
 
 export default NewPost;
